d02303dcced606b11e20e3f6f1b016d94bc3a7d6
Yevhen <wesatellite@bigmir.net>
2018-04-30 10:03:00 +0300

    Add files via upload

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+
+    public Transform[] backgrounds;         // Array (list) of all of the back and foregrounds to be parallaxed
+    private float[] parallaxScales;         // The proportion of the camera's movement to move the backgrounds by
+    public float smoothing = 1f;            // How smooth the parallax is going to be. Must be above 0;
+
+    private Transform cam;                  // Reference to the main cameras transform
+    private Vector3 previousCamPosition;    // the position of the camera in the previous frame
+
+    // Is called before Start(). Great for references.
+    void Awake()
+    {
+        // Set up  camera the reference
+        cam = Camera.main.transform;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        // The previous frame had the current frame's camera position
+        previousCamPosition = cam.position;
+
+        //assigning coresponding parallaxScales
+        parallaxScales = new float[backgrounds.Length];
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            parallaxScales[i] = backgrounds[i].position.z - 1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //for each background
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            // the parallax is the opposite of the camera movement because the previous frame multiplied by the scale
+            float parallax = (previousCamPosition.x - cam.position.x) * parallaxScales[i];
+
+            // set a target x position which is the current position plus the parallax
+            float backgroundTargetPosX = backgrounds[i].position.x + parallax;
+
+            // create a target position which is the background's current position which it's target x position (no y parallax)
+            Vector3 backgroundTargetPosition = new Vector3(backgroundTargetPosX, backgrounds[i].position.y, backgrounds[i].position.z);
+
+            // fade between current position and the target position using lerp
+            backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPosition, smoothing * Time.deltaTime);
+        }
+
+        // set the previousCamPosition to the camera's position at the end of the frame
+        previousCamPosition = cam.position;
+    }
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float moveSpeed;
+    public float jumpHeight;
+    public float gravity;
+
+    public float pixelSize;
+
+    public LayerMask solid;
+
+    private float horisontalSpeed;
+    private float verticalSpeed;
+
+    private bool keyLeft;
+    private bool keyRight;
+    private bool keyUp;
+    private bool keyDown;
+    private bool keyJump;
+    private bool keyAction;
+
+    private bool onGround;
+
+    private Vector2 botLeft; //Coordinates of an angles
+    private Vector2 botRight;
+    private Vector2 topLeft;
+    private Vector2 topRight;
+
+    private Rigidbody2D myRigidBody2D;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+    
+	// Update is called once per frame
+	void Update () {
+        CalculateBounds();
+        onGround = CheckCollision(botLeft, Vector2.down, pixelSize, solid) || CheckCollision(botRight, Vector2.down, pixelSize, solid);
+
+        GetInput();
+        Move();
+	}
+
+    //Key is pressed or not
+    void GetInput()
+    {
+        keyLeft = Input.GetKey(KeyCode.LeftArrow);
+        keyRight = Input.GetKey(KeyCode.RightArrow);
+        keyUp = Input.GetKey(KeyCode.UpArrow);
+        keyDown = Input.GetKey(KeyCode.DownArrow);
+        keyJump = Input.GetKeyDown(KeyCode.Z);
+        keyAction = Input.GetKey(KeyCode.X);
+    }
+
+    void Move()
+    {
+        if (keyLeft) horisontalSpeed = -moveSpeed * Time.deltaTime;
+        if (keyRight) horisontalSpeed = moveSpeed * Time.deltaTime;
+        if((!keyLeft && !keyRight) || (keyLeft && keyRight)) horisontalSpeed = 0;
+
+        if (keyJump && onGround) 
+        {
+            verticalSpeed = jumpHeight;
+            onGround = false;
+        }
+
+        if (!onGround) verticalSpeed -= gravity * Time.deltaTime;
+
+        //cheking floor under the legs
+        if ((verticalSpeed < 0) && (CheckCollision(botLeft, Vector2.down, Mathf.Abs(verticalSpeed), solid) || CheckCollision(botRight, Vector2.down, Mathf.Abs(verticalSpeed), solid)))
+        {
+            float dist1 = CheckCollisionDistance(botLeft, Vector2.down, Mathf.Abs(verticalSpeed), solid);
+            float dist2 = CheckCollisionDistance(botRight, Vector2.down, Mathf.Abs(verticalSpeed), solid);
+            if (dist1 <= dist2) verticalSpeed = -dist1;
+            else verticalSpeed = -dist2;
+
+            transform.position = new Vector2(transform.position.x, transform.position.y + verticalSpeed+ pixelSize/2);
+            verticalSpeed = 0; 
+        }
+
+        transform.position = new Vector2(transform.position.x + horisontalSpeed, transform.position.y + verticalSpeed);
+    }
+
+    private bool CheckCollision(Vector2 raycastOrigin, Vector2 direction, float distance, LayerMask layer) 
+    {
+        return Physics2D.Raycast(raycastOrigin, direction, distance, layer);
+    }
+
+    private float CheckCollisionDistance(Vector2 raycastOrigin, Vector2 direction, float distance, LayerMask layer) 
+    {
+        int i = 0;
+        while (Physics2D.Raycast(raycastOrigin, direction, distance, layer)) 
+        {
+            i++;
+
+            if (distance > pixelSize) distance -= pixelSize;
+            else distance = pixelSize;
+
+            if (i > 1000) return 0;
+        }
+        return distance;
+    }
+
+    private void CalculateBounds() 
+    {
+        Bounds b = GetComponent<BoxCollider2D>().bounds;
+        topLeft = new Vector2(b.min.x, b.max.y);
+        topRight = new Vector2(b.max.x, b.max.y);
+        botLeft = new Vector2(b.min.x, b.min.y);
+        botRight = new Vector2(b.max.x, b.min.y);
+    }
+}
